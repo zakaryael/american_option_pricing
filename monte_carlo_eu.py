750a74b5eefee57dcde6f91349dbd146d6f52137
El Khiyati Zakarya <zakaria.elkhiyati@gmail.com>
2020-05-02 18:00:15 +0200

    Update monte_carlo_eu.py

--- monte_carlo_eu.py
+++ monte_carlo_eu.py
@@ -11,6 +11,18 @@ class mc_eu(Option):
         CI = [delta + q * sqrt(Var/n) for q in norm.interval(level)]
         return {"option price":delta, "confidence interval":CI, "variance":Var}
     
+    def evolution(self, n, level = 0.95):
+        x = asset.simulate(n)
+        y = self.payoff(x)
+        nvec = 
+        delta = np.cumsum(y) / np.array(range(1, n))
+        Var <- ( np.cumsum(y**2) -  * (delta ^ 2)) / 0:(n-1); Var[1] <- 0
+        i1 <- delta - q * sqrt (Var / 1:n)
+        i2 <- delta + q * sqrt (Var / 1:n)
+        t <- 1:n
+        plot(t, delta, type = 'l')
+        lines(t, i1, col = 'red', type = 'l')
+        lines(t, i2, col = 'blue', type = 'l')
 
 market = Market(0, 0.2, 1)
 asset = Asset(market, 100)
